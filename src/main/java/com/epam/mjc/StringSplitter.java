e63f552012b176a66ff32185b0ef6a6a298b6ccb
ZakharSytoi <zakharsytoi@gmail.com>
2023-04-11 19:32:48 +0200

    Task 1 completed

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,18 +1,39 @@
 package com.epam.mjc;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class StringSplitter {
 
     /**
      * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
      *
-     * @param source source string
+     * @param source     source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
+
+
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> result = new ArrayList<>();
+        String tmpString = source;
+
+        for (String delimiter : delimiters) {
+            StringBuilder sb = new StringBuilder();
+            for (String i : tmpString.split(delimiter)) {
+                sb.append(i).append(" ");
+            }
+            sb.deleteCharAt(sb.length() - 1);
+            tmpString = sb.toString();
+        }
+        StringBuilder sb = new StringBuilder(tmpString);
+        if(sb.charAt(0) == ' ') sb.deleteCharAt(0);
+        for (int i = 0; i < sb.length() - 1; i++) {
+            while (sb.charAt(i) == ' ' && sb.charAt(i + 1) == ' ') {
+                sb.deleteCharAt(i + 1);
+            }
+        }
+        tmpString = sb.toString();
+        Collections.addAll(result, tmpString.split(" "));
+        return result;
     }
 }
